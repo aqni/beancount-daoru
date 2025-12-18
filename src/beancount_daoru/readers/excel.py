5878f8520466d3b0c98fb6c12f736c30e4457e71
aqni <aqnin@outlook.com>
2025-12-18 20:49:46 +0800

    build(pyexcel): 使用只读的 xlsx 读取器以减少依赖
    同时微调了Excel读取器的实现，完善了文档注释

--- src/beancount_daoru/readers/excel.py
+++ src/beancount_daoru/readers/excel.py
@@ -8,9 +8,13 @@ from collections.abc import Iterator
 from pathlib import Path
 
 import pyexcel
-from typing_extensions import override
+from typing_extensions import TypedDict, Unpack, override
 
 from beancount_daoru import reader
+
+
+class _ReaderKwargs(TypedDict, total=False):
+    encoding: str
 
 
 class Reader(reader.Reader):
@@ -24,27 +28,27 @@ class Reader(reader.Reader):
         self,
         /,
         header: int,
-        encoding: str | None = None,
+        **kwargs: Unpack[_ReaderKwargs],
     ) -> None:
         """Initialize the Excel reader.
 
         Args:
-            schema: Pydantic model or TypedDict defining the record structure.
             header: Number of header rows to skip before data.
-            encoding: Text encoding to use when reading the file.
+            kwargs: Additional keyword arguments passed to pyexcel.
         """
         self.header = header
-        self.encoding = encoding
+        self.kwargs = kwargs
 
     @override
     def read_captions(self, file: Path) -> Iterator[str]:
         for row in pyexcel.get_array(
             file_name=file,
-            encoding=self.encoding,
             row_limit=self.header,
             auto_detect_int=False,
             auto_detect_float=False,
             auto_detect_datetime=False,
+            skip_empty_rows=True,
+            **self.kwargs,
         ):
             yield from row
 
@@ -52,12 +56,12 @@ class Reader(reader.Reader):
     def read_records(self, file: Path) -> Iterator[dict[str, str]]:
         for row in pyexcel.iget_records(
             file_name=file,
-            encoding=self.encoding,
             start_row=self.header,
             auto_detect_int=False,
             auto_detect_float=False,
             auto_detect_datetime=False,
             skip_empty_rows=True,
+            **self.kwargs,
         ):
             yield {
                 self.__convert(key): self.__convert(value) for key, value in row.items()
